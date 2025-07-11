8862de99a1bc74dfb2598ea8c3ca95535b3dc396
airforceleather <airforceleather@gmail.com>
2025-07-11 11:28:48 +0900

    add

---
+++ CodingTest/src/step1/Main1.java
@@ -0,0 +1,16 @@
+package step1;
+
+/*
+ * 날짜 : 2025/07/10
+ * 이름 : 김대현
+ * 내용 : 백준 1단계 1번 문제 - Hello World
+ * */
+
+public class Main1 {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World");
+	}
+
+}
