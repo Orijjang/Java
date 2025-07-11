a1564b3901c903d2d13c37351abdf045350fada7
airforceleather <airforceleather@gmail.com>
2025-07-11 11:35:44 +0900

    오버로드 메서드 수정

--- ch04/src/sub4/MethodOverloadTest.java
+++ ch04/src/sub4/MethodOverloadTest.java
@@ -15,11 +15,14 @@ public class MethodOverloadTest {
 		int r2 = add(2,3);
 		double r3 = add(1.1, 3.14);
 		String r4 = add("Hello", "Java");
+		String r5 = add("number_", 1);
+		
 		
 		System.out.println("r1 : " + r1);
 		System.out.println("r2 : " + r2);
 		System.out.println("r3 : " + r3);
 		System.out.println("r4 : " + r4);
+		System.out.println("r5 : " + r5);
 		
 		
 	}
@@ -42,4 +45,9 @@ public class MethodOverloadTest {
 		return a + b;
 	}
 
+	public static String add(String a, int b) {
+		
+		return a+b;
+	}
+	
 }
