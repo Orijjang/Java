12d7e159abeae2927797c3679544d6d3054a2c9b
airforceleather <airforceleather@gmail.com>
2025-07-18 13:59:11 +0900

    add javatest

--- JavaTest/src/test4/sub4/Book.java
+++ JavaTest/src/test4/sub4/Book.java
@@ -2,31 +2,16 @@ package test4.sub4;
 
 public class Book {
 
-	public static void main(String[] args) {
-		
-		Book book = new Book("자바 프로그래밍", "제임스 고슬링", "101-1234-1001");
-		
-		Member member = new Member("홍길동", "A001");
-		
-		book.getBookInfo();
-		System.out.println("======================");
-		
-		member.borrowBook(book);
-		System.out.println("======================");
-		
-		book.getBookInfo();
-		System.out.println("======================");
-		
-		member.getMemberInfo();
-		System.out.println("======================");
-		
-		member.borrowBook(book);
-		System.out.println("======================");
+	private String title;
+	private String author;
+	private String isbn;
+	private boolean isBorrowd;
 	
-		member.returnBook(book);
-		System.out.println("======================");
-	
-		member.getMemberInfo();
-		System.out.println("======================");
+	public Book(String title, String author, String isbn) {
+		this.title =title;
+		this.author = author;
+		this.isbn = isbn;
+		this.isBorrowd = false;
 	}
+	
 }
