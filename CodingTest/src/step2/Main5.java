d6249737ca4299b3580c92233822e7eb12089cdc
airforceleather <airforceleather@gmail.com>
2025-07-25 12:20:56 +0900

    add codingtest

--- CodingTest/src/step2/Main5.java
+++ CodingTest/src/step2/Main5.java
@@ -29,6 +29,20 @@ public class Main5 {
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
-	}
+		
+		int hour = sc.nextInt();
+		int minute = sc.nextInt();
+		
+		minute -= 45; 
+		
+		if(minute < 0) {
+			minute += 60;
+			hour -=1;
+			if(hour<0) {
+				hour = 23;
+			}
+		}
+		System.out.println(hour + " "+minute);
 	
+}
 }
