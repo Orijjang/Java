ce24c4976491fafd347568f075f138be50b20e33
airforceleather <airforceleather@gmail.com>
2025-07-25 12:19:12 +0900

    add ch10

---
+++ ch10/src/sub2/ThreadInterruptTest.java
@@ -0,0 +1,45 @@
+package sub2;
+/*날짜 : 2025/07/24
+ *이름 : 김대현
+  내용 : 자바 스레드 상태 실습하기
+   */
+
+class SubThread extends Thread {
+	public void run() {
+		int i = 1;
+		
+		try {
+			while(true) {
+				System.out.println("i : " + i);
+				i++;
+			
+				Thread.sleep(1000);
+
+			}
+		}catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+}
+public class ThreadInterruptTest {
+
+	public static void main(String[] args) {
+		
+		SubThread sub = new SubThread();
+		
+		//작업 스레드 실행
+		sub.start();
+		
+		try {
+			//메인스레드 5초대기
+			Thread.sleep(1000*5);
+		}catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+		//작업 스레드 일시정지 발생시키기
+		sub.interrupt();
+		
+		System.out.println("프로그램 종료...");
+	}
+}
